f48578eb91ee93c12daa1c466d077df06fbcaeb2
SN1164 <115774815+SN1164@users.noreply.github.com>
2023-02-10 10:13:32 +0400

    T

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ let products = [
      "title": "Cat Food, 25lb bag",
      "description": "A 25 pound bag of <em>irresistible</em>, ...",
      "price": 20.00,
-     "image": "images/cat-food.jpeg",
+     "image": "pwa/images/cat-food.jpeg",
      "availableInventory": 10,
      "rating": 2    },   
      
@@ -11,7 +11,7 @@ let products = [
      "title": "Yarn",
      "description": "Yarn your cat can play with for a very ...",
      "price": 2.99,
-     "image": "images/cat-food.jpeg",
+     "image": "pwa/images/cat-food.jpeg",
      "availableInventory": 7,
      "rating": 3    
 }
--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ var cacheFiles = [
     'index.html',
     'product.js',
     'petstore.webmanifest',
-    'images/cat-food.jpeg'
+    'pwa/images/cat-food.jpeg'
 ];
 
 self.addEventListener('install', (e) => {
