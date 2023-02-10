05f176cf7764febf7e68a40ec507eb184969cace
SN1164 <115774815+SN1164@users.noreply.github.com>
2023-02-10 10:04:18 +0400

    PWA

---
+++ products.js
@@ -0,0 +1,18 @@
+let products = [   
+     {"id": 1001,
+     "title": "Cat Food, 25lb bag",
+     "description": "A 25 pound bag of <em>irresistible</em>, ...",
+     "price": 20.00,
+     "image": "images/cat-food.jpeg",
+     "availableInventory": 10,
+     "rating": 2    },   
+     
+     {"id": 1002,
+     "title": "Yarn",
+     "description": "Yarn your cat can play with for a very ...",
+     "price": 2.99,
+     "image": "images/cat-food.jpeg",
+     "availableInventory": 7,
+     "rating": 3    
+}
+]
